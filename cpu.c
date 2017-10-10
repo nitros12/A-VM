5f0e6ef58a15f2163f68671cd0b27850e6c30c0f
nitros12 <ben@bensimms.moe>
2017-10-10 12:16:06 +0100

    aaa laptop on 3%

--- cpu.c
+++ cpu.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include "cpu.h"
 
 CPU *make_cpu(size_t mem_size) {
   CPU *cpu = malloc(sizeof(CPU) + sizeof(uint8_t) * mem_size);
   cpu->cycles = 0;
-  cpu->regs.cur = 0;
+  cpu->regs[cur].u8 = 0;
   return cpu;
 }
 
@@ -16,31 +18,56 @@ CPU *make_cpu(size_t mem_size) {
 * length of operands is dependant on instruction
 */
 instr_fp decode_instr(CPU *cpu) {
-  uint8_t op = cpu->memory[cpu->regs.cur];
+  uint8_t op = cpu->memory[cpu->regs[cur].u8];
   return instruction_set[op-1];
 }
 
-uint8_t getOperand1(CPU *cpu) {
-  return GET_OPERAND(uint8_t);
+cpu_union cpu_getloc(CPU *cpu, uint16_t loc) {
+    bool reg = TO_REG(loc);
+    bool deref = TO_LOC(loc);
+    cpu_union res = {.u8 = loc & 0b0011111111111111};
+
+    if (reg)
+        res.u8 = cpu->regs[res.u8].u8;
+    if (deref)
+        res.u8 = *(uint64_t *)&cpu->memory[res.u8];
+    return res;
 }
 
-uint16_t getOperand2(CPU *cpu) {
-  return GET_OPERAND(uint16_t);
-}
-
-uint32_t getOperand3(CPU *cpu) {
-  return GET_OPERAND(uint32_t);
-}
-
-uint64_t getOperand4(CPU *cpu) {
-  return GET_OPERAND(uint64_t);
-}
 
 /*
 * :param loc: start location to set to
 * :param size: size to set
 * :param src: source to copy from
  */
-void cpu_setloc(CPU *cpu, uint16_t loc, size_t size, uint8_t *src) {
-  /* TODO:  implement setloc to set the register/ memory location on the cpu*/
+void cpu_setloc(CPU *cpu, uint16_t loc, cpu_size size, cpu_union src) {
+
+    bool reg = TO_REG(loc);
+    bool deref = TO_LOC(loc);
+
+    if (deref) {
+        loc = cpu_getloc(cpu, loc).u2;
+        switch (size) {
+            case w1: *(uint8_t *)&cpu->memory[loc] = src.u1; break;
+            case w2: *(uint16_t *)&cpu->memory[loc] = src.u2; break;
+            case w4: *(uint32_t *)&cpu->memory[loc] = src.u4; break;
+            case w8: *(uint64_t *)&cpu->memory[loc] = src.u8; break;
+        }
+    } else {
+        loc &= 0b0011111111111111;
+        if (reg)
+            switch (size) {
+                case w1: cpu->regs[loc].u1 = src.u1; break;
+                case w2: cpu->regs[loc].u2 = src.u2; break;
+                case w4: cpu->regs[loc].u4 = src.u4; break;
+                case w8: cpu->regs[loc].u8 = src.u8; break;
+            }
+        else
+            switch (size) {
+                case w1: *(uint8_t *)&cpu->memory[loc] = src.u1; break;
+                case w2: *(uint16_t *)&cpu->memory[loc] = src.u2; break;
+                case w4: *(uint32_t *)&cpu->memory[loc] = src.u4; break;
+                case w8: *(uint64_t *)&cpu->memory[loc] = src.u8; break;
+            }
+    }
 }
--- cpu.h
+++ cpu.h
@@ -1,31 +1,41 @@
 #include <inttypes.h>
 #include <stdlib.h>
 
-#define GET_OPERAND(X) (((X *)cpu->memory)[(*(X *)&cpu->regs.cur)++])
+#define GET_OPERAND(X) (((X *)cpu->memory)[((*(X *)&cpu->regs)[registers.cur])++])
 
-#define REG(x) union {\
-    int64_t  s8; \
-    uint64_t u8; \
-    int32_t  s4; \
-    uint32_t u4; \
-    int16_t  s2; \
-    uint16_t u2; \
-    int8_t   s1; \
-    uint8_t  u1; \
-  } (x)
+#define TO_REG(X) (0b100000000000 & X)
+#define TO_LOC(X) (0b010000000000 & X)
 
-typedef struct {
-  REG(aaa);
-  REG(bbb);
-  REG(stk);
-  REG(bsp);
-  REG(acc);
-  size_t cur;
+typedef union {
+    int64_t  s8;
+    uint64_t u8;
+    int32_t  s4;
+    uint32_t u4;
+    int16_t  s2;
+    uint16_t u2;
+    int8_t   s1;
+    uint8_t  u1;
+  } cpu_union;
+
+typedef enum {
+    w1 = 1,
+    w2 = 2,
+    w4 = 4,
+    w8 = 8
+} cpu_size;
+
+typedef enum {
+  aaa = 0,
+  bbb,
+  stk,
+  bsp,
+  acc,
+  cur,
 } registers;
 
 typedef struct {
   uint64_t cycles;
-  registers regs;
+  cpu_union regs[6];
   uint8_t memory[];
 } CPU;
 
